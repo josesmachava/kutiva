e16a847dcb8165f0c859efbf9e16025f8674eeb0
jose machava <jose.s.machava@gmail.com>
2019-03-19 23:12:01 +0200

    remove labels in forms

--- enrollment/forms.py
+++ enrollment/forms.py
@@ -5,5 +5,19 @@ from .models import Student
 
 class StudentForm(ModelForm):
     class Meta:
+
+
         model = Student
         fields = '__all__'
+        labels = {
+            'name': '',
+            'surname' : '',
+            'phone_number':'',
+            'email':''
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Nome'}),
+            'surname': forms.TextInput(attrs={'placeholder': 'Apelido'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Numero de telefone'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Email'})
+        }
