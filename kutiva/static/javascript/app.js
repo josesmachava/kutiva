7cbdf13453594a63e772810b6b988de25c6ea3ee
jose machava <jose.s.machava@gmail.com>
2019-03-31 12:02:25 +0200

    implemment navegation

--- kutiva/static/javascript/app.js
+++ kutiva/static/javascript/app.js
@@ -1,6 +1,11 @@
 "use strict"
+$('document').ready(function () {
 
-$('.optionstyle a').click(function(){
-    $(this).parent().find('.optionstyle').css('border-bottom','#ffffff');
-    $(this).css('border-bottom','3px solid blue');
+
+    $('.menuIcon').click(function () {
+        $('.mobileMenu').show();
+        $('.menuIcon').hide();
+    })
+
+
 });
