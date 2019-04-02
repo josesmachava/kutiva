01725e9cd817bb195f11287d11080fbcbf8b96cc
jose machava <jose.s.machava@gmail.com>
2019-04-02 23:59:02 +0200

    curse details

--- kutiva/static/javascript/navegation.js
+++ kutiva/static/javascript/navegation.js
@@ -1,11 +1,11 @@
-"use strict"
+"use strict";
 $('document').ready(function () {
 
 
     $('.menuIcon').click(function () {
         $('.mobileMenu').show();
         $('.menuIcon').hide();
-    })
+    });
      $('.closeIcon').click(function () {
         $('.mobileMenu').hide();
         $('.menuIcon').show();
