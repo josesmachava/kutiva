201fb4c635048f38e529b0afad032f61f2c2d63d
jose machava <jose.s.machava@gmail.com>
2019-04-25 00:48:23 +0200

    add user settings

--- kutiva/static/javascript/navegation.js
+++ kutiva/static/javascript/navegation.js
@@ -1,11 +1,21 @@
 "use strict";
 $('document').ready(function () {
+	$( ".settings" ).hide();
+	$("main").click(function(){
+			$( ".settings" ).hide();
 
 
+	})
     $('.menuIcon').click(function () {
         $('.mobileMenu').show();
         $('.menuIcon').hide();
     });
+    $( ".user-name" ).click(function() {
+	  $( ".settings" ).toggle()
+	});
+    
+
+
      $('.closeIcon').click(function () {
         $('.mobileMenu').hide();
         $('.menuIcon').show();
