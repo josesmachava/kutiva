47be8abbef60f9f5b4f53ff3a72fb09dbcefaeea
jeremiah <jeremiasfondo@gmail.com>
2019-04-28 14:07:08 +0200

    models payment

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from datetime import timezone
 
+
+from django.utils import timezone
+from django.db import models
+from kutiva import settings
 # Create your models here.
+
+class Payment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    last_date = models.DateTimeField()
+    mount = models.DecimalField(max_digits=6, decimal_places=2)
+    created_date = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=True)
