ec7539421827086a3c4975771d6f0e5379af6756
jose machava <jose.s.machava@gmail.com>
2019-04-28 20:48:51 +0200

    fix indentation

--- kutiva/settings.py
+++ kutiva/settings.py
@@ -136,7 +136,7 @@ except ImportError:
     ALLOWED_HOSTS = ["*"]
 
 
-   DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
