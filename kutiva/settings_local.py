a69e57235e03c756c80eb0e167f161e935441cab
jose machava <jose.s.machava@gmail.com>
2019-04-28 20:31:26 +0200

    add local settings

---
+++ kutiva/settings_local.py
@@ -0,0 +1,9 @@
+# Please copy this as a settings_local.py on the same directory and ensure to not add to version control, i.e. git ignore this file. Adjust the variables to meet your own settings
+#To automate Local and Production server settings
+import socket
+
+if socket.gethostname() == 'josemachava-ThinkPad-T440':	#ensure your local machine hostname is used
+    DEBUG = True
+    # DATABASES['default'] = DATABASES['default']	#for tests, comment out upon upgrade
+    
+Has_Local_Settings = True
