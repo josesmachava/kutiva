c61405b1edad20a1a9c00d680eac56daaa5f4260
jose machava <jose.s.machava@gmail.com>
2019-04-29 15:40:07 +0200

    add change

--- cms/models.py
+++ cms/models.py
@@ -14,22 +14,21 @@ class Lesson(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+         return str(self.name)
 
 
 class Subcategory(models.Model):
     name = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.name
-
+       return str(self.name)
 
 class Category(models.Model):
     name = models.CharField(max_length=1000)
     subcategory = models.ForeignKey(Subcategory, on_delete='CASCADE', blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Chapter(models.Model):
