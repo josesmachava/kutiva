32cbf61b01d632f8da5a944e3293ca9fd6edbc45
= <jose.s.machava@gmail.com>
2020-03-22 23:38:34 +0200

    add changes

--- kutiva/static/javascript/app.js
+++ kutiva/static/javascript/app.js
@@ -1,4 +1,12 @@
 
 $(document).ready(function(){
    //   $('.testimonials').slick();
+    $('.search-mobile').hide();
+    $('.search-icon').click(function () {
+              $('.search-mobile').toggle('slow');
+    })
+    window.onscroll = function(){
+                          $('.search-mobile').hide();
+
+    }
 })
