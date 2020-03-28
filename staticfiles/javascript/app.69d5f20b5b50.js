93b1ebac08a995a0a315d9df07e23ff01ae8bcc4
= <jose.s.machava@gmail.com>
2020-03-28 16:11:07 +0200

    add jade/pug support

---
+++ staticfiles/javascript/app.69d5f20b5b50.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+   //   $('.testimonials').slick();
+    $('.search-mobile').hide();
+    $('.user-menu').hide();
+    $('.search-icon').click(function () {
+              $('.search-mobile').toggle('slow');
+    })
+    $('.user-icon').click(function () {
+              $('.user-menu').toggle();
+    })
+    window.onscroll = function(){
+                          $('.search-mobile').hide();
+
+    }
+      window.onscroll = function(){
+                          $('.user-menu').hide();
+
+    }
+})
