0082a70d3577e2c812f8de777c07a6744f2dc4cf
= <jose.s.machava@gmail.com>
2020-04-02 11:30:21 +0200

    add missing comma

--- kutiva/settings.py
+++ kutiva/settings.py
@@ -90,7 +90,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
