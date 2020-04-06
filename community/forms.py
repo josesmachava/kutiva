80c0ed6477040c49480fdea2157fb0baeefdb4c4
= <jose.s.machava@gmail.com>
2020-04-06 11:33:37 +0200

    add humanize time

--- community/forms.py
+++ community/forms.py
@@ -1,8 +1,19 @@
 from .models import Comment
 from django import forms
+from .models import Post
 
 
-class CommentForm(forms.ModelForm):
+#class CommentForm(forms.ModelForm):
+   # class Meta:
+   #     model = Comment
+   #     fields = ('body')
+
+
+
+
+
+class PostForm(forms.ModelForm):
+
     class Meta:
-        model = Comment
-        fields = ('body')
+        model = Post
+        fields = ('text',)
--- community/views.py
+++ community/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
 # Create your views here.
-
+from community.models import Post
+from .forms import PostForm
 
 def post_list(request):
-    return render(request, 'community/post_list.html', {})
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+            #return redirect('post_list', pk=post.pk)
+    else:
+        posts = Post.objects.all().order_by('created_date')
+
+        form = PostForm()
+
+    return render(request, 'community/post_list.html', {'posts': posts,  'form': form})
